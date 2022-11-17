ae644cec69ceeaf3b46c08a8b5b0667f5cb19a6b
vnemchev <vnemchev@gmail.com>
2022-11-17 23:22:11 +0100

    Nav component and core module

--- sustainsounds/src/app/app.module.ts
+++ sustainsounds/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventsModule } from './events/events.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     EventsModule,
+    CoreModule,
     HttpClientModule,
     BrowserAnimationsModule,
   ],
