2bfb3d50c05aae42c819fbec2e6e780e07dc632e
vnemchev <vnemchev@gmail.com>
2022-11-18 22:11:37 +0100

    Routing added in nav

--- sustainsounds/src/app/core/core.module.ts
+++ sustainsounds/src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav/nav.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [NavComponent],
-  imports: [CommonModule, MatToolbarModule],
+  imports: [CommonModule, RouterModule, MatToolbarModule],
   exports: [NavComponent],
 })
 export class CoreModule {}
